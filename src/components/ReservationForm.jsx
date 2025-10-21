13d1f0d24bede826fc5639fa6df658c3304280a6
Tharun Kalugalla <krtkalugalla@gmail.com>
2025-10-22 00:55:41 +0530

    modified:   src/components/ReservationForm.jsx

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import map from "@/assets/map.png";
