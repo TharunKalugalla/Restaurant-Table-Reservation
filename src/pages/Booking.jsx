2675b08a6ef6339f950621d4dbceb31715fd0a2e
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-11-04 15:41:44 +0530

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -339,12 +339,7 @@ export default function Booking() {
 
                 </div>
 
-                <p className="text-xs text-muted-foreground mt-6 leading-relaxed">
-                  Lorem ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took
-                </p>
+
               </div>
             )}
 
