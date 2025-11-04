6514dcbf1469d86ed1c04bff57fde18776f1bf72
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-11-04 15:50:31 +0530

    Update ReservationCompleteMassege.jsx

--- src/components/ReservationCompleteMassege.jsx
+++ src/components/ReservationCompleteMassege.jsx
@@ -56,11 +56,7 @@ export default function ReservationCompleteMassege({ formData, bookingData }) {
       </div>
 
       {/*--------------------------------------------------------- Footer Text -------------------------------------------------*/}
-      <p className="text-sm text-muted-foreground text-center leading-relaxed">
-        Lorem ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took
-      </p>
+
     </div>
   );
 }
